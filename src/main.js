964f634315fffa13a34a4ede5b76aa38ce6737b6
Joeyyyys <josephgiler2@gmail.com>
2023-11-03 11:56:54 -0400

    created a function to showcase the movies data being displayed with DOM

--- src/main.js
+++ src/main.js
@@ -4,3 +4,41 @@ let defaultMovies = movies;
 
 console.log("ufggn");
 
+// DOM
+const createDefaultMovies = (movieTitle, criticScore, audienceScore, domesticTotal, genre) => {
+    const ul = document.querySelector("#default-movies");
+    const li = document.createElement("li");
+    const h2 = document.createElement("h2");
+    const criticInfo = document.createElement("p");
+    const audienceInfo = document.createElement("p");
+    const domesticInfo = document.createElement("p");
+    const genreaudienceInfo = document.createElement("p");
+
+    h2.textContent = movieTitle;
+    criticInfo.textContent = `Critic Score: ${criticScore}`;
+    audienceInfo.textContent = `Audience Score: ${audienceScore}`;
+    domesticInfo.textContent = `Domestic Total: ${domesticTotal}`;
+    genreaudienceInfo.textContent = `Genre: ${genre}`;
+
+
+    li.append(h2, criticInfo, audienceInfo, domesticInfo, genreaudienceInfo)
+    ul.append(li)
+}
+
+
+const displayDefaultMovies = () => {
+    try {
+        movies.forEach(
+            movie => createDefaultMovies(movie.title, movie.criticScore, movie.audienceScore, movie.domestic, movie.genre)
+        )
+    } catch (error){
+        console.log(error)
+    }
+}
+
+displayDefaultMovies();
+
+
+
+// localstorage functions 
+
