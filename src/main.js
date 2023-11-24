6ec71eb5f75cca4ef80be8d1c6612e2029a96ac7
Joeyyyys <josephgiler2@gmail.com>
2023-11-24 12:58:53 -0500

    made a function to handle all our submit form data and then use DOM to display that movie data, and store it inside of our local storage funcitons, added a reset default movie function with deletes all of the movies inside of local storage, then running the original movie data, and a main function handler that allows us to call all of our functions in one place

--- src/main.js
+++ src/main.js
@@ -26,7 +26,6 @@ const createDefaultMovies = (movieTitle, criticScore, audienceScore, domesticTot
     ul.append(li)
 }
 
-
 const displayDefaultMovies = () => {
     try {
         movies.forEach(
@@ -36,10 +35,6 @@ const displayDefaultMovies = () => {
         console.log(error)
     }
 }
-
-displayDefaultMovies();
-
-
 
 // localstorage functions 
 const setLocalStorageKey = (key, value) => {
@@ -65,8 +60,84 @@ export const addMovies = (newMovie) => {
 }
 
 export const initDefaultMoviesIfEmpty = () => {
-    if (!getMovies.length) setMovies(defaultMovies)
+
+    const storedMovies = getMovies();
+    
+    if (storedMovies.length === 0) setMovies(defaultMovies)
 }
 
-initDefaultMoviesIfEmpty();
 console.log(localStorage.movies) // to see all our movies inside of localstorage as a string
+
+// Form Submission 
+const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const movieTitle = formData.get('movie-title');
+    const criticScore = formData.get('critic-score'); 
+    const audienceScore = formData.get('audience-score');
+    const domesticTotal = formData.get('domestic-gross-sales');
+    const genre = formData.get('genre');
+
+    if (!movieTitle || !criticScore || !audienceScore || !domesticTotal || !genre) {
+        alert('Please complete all fields before submitting.');
+        return;
+    }
+
+    const newMovie = {
+        title: movieTitle,
+        criticScore: criticScore,
+        audienceScore: audienceScore,
+        domestic: domesticTotal,
+        genre: genre
+    };
+
+    createDefaultMovies(newMovie.title, newMovie.criticScore, newMovie.audienceScore, newMovie.domestic, newMovie.genre);
+
+    addMovies(newMovie);
+
+    e.target.reset();
+
+}
+// DOM to display all the movies, with the newly added movies from our form submission
+const displayAddedMovies = () => {
+    try {
+        const addedMovies = getMovies();
+        addedMovies.forEach(movie => {
+            createDefaultMovies(movie.title, movie.criticScore, movie.audienceScore, movie.domestic, movie.genre);
+        });
+    }
+     catch (error){
+        console.log(error)
+    }
+}
+
+// Function to clear all our movies inside of localstorage and then running our movie-data DOM function to display the original movies
+const resetToDefaultMovies = () => {
+
+    setMovies(defaultMovies);
+
+    const ul = document.querySelector("#default-movies");
+    ul.innerHTML = "";
+
+    displayDefaultMovies();
+};
+
+// Allows for all our functions to be called in one place
+const main = () => {
+    initDefaultMoviesIfEmpty();
+
+    displayDefaultMovies();
+
+    displayAddedMovies();
+    
+    const form = document.querySelector("#new-movie-adder");
+    form.addEventListener("submit", handleSubmit);  
+
+    const resetButton = document.querySelector("#default-button");
+    resetButton.addEventListener("click", resetToDefaultMovies);
+};
+
+main()
+
+
